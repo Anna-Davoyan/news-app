68f0921bad1721a705996b85ae16f33566eeacff
Davoyan Anna <davoyanan@gmail.com>
2020-07-11 15:16:47 +0400

    build

---
+++ build/precache-manifest.e45d8e934d4284f1bff065e8e2e6d0a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590cd140f7a76dfc1f78798d264feb53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6b16f044935e306756",
+    "url": "/static/css/main.e3212d35.chunk.css"
+  },
+  {
+    "revision": "8e7ec59bb794a2d756d5",
+    "url": "/static/js/2.843fde50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.843fde50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6b16f044935e306756",
+    "url": "/static/js/main.2ea6b273.chunk.js"
+  },
+  {
+    "revision": "f76bbb8b006909bb2d5d",
+    "url": "/static/js/runtime-main.b98ba16f.js"
+  },
+  {
+    "revision": "4abf70033ab6228047982278022d559b",
+    "url": "/static/media/logo.4abf7003.png"
+  }
+]);
